2d8fbb64218afcf9bb1bb712c6a88af143b48b56
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2023-01-12 20:45:43 +0530

    SOME BAISC EDITING ON HOMPAGE

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,30 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true
+  },
+  spaceBetween: 60,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+
+
+
+
+
 (function() {
   "use strict";
 
